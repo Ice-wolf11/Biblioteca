c98685f74bd112d80e964fa72ca6b8ccd83e70f8
Andrew <andrew@gmail.com>
2024-11-09 23:56:16 -0500

    Importando la plantilla

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+/*
 Route::get('/', function () {
     return view('welcome');
+});*/
+Route::get('/', function () {
+    return view('template');
+});
+Route::get('/login', function () {
+    return view('auth.login');
+});
+Route::get('/register', function () {
+    return view('auth.register');
+});
+Route::get('/forgot-password', function () {
+    return view('auth.forgot-password');
+});
+Route::get('/401', function () {
+    return view('pages.401');
+});
+Route::get('/404', function () {
+    return view('pages.404');
+});
+Route::get('/500', function () {
+    return view('pages.500');
 });
